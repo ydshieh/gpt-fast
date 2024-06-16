0c574879c9ce676e12793a76e3c4c42b2676bd94
ydshieh <ydshieh@users.noreply.github.com>
2024-06-16 14:57:32 +0200

    dynamic

--- generate.py
+++ generate.py
@@ -13,6 +13,10 @@ import torch
 import torch._dynamo.config
 import torch._inductor.config
 
+torch.backends.cuda.enable_flash_sdp(False)
+torch.backends.cuda.enable_mem_efficient_sdp(False)
+torch.backends.cuda.enable_math_sdp(True)
+
 def device_sync(device):
     if "cuda" in device:
         torch.cuda.synchronize(device)
@@ -56,24 +60,30 @@ def sample(logits, temperature: float = 1.0, top_k: Optional[int] = None):
 
 def prefill(model: Transformer, x: torch.Tensor, input_pos: torch.Tensor, **sampling_kwargs) -> torch.Tensor:
     # input_pos: [B, S]
-    logits = model(x, input_pos)
+    _length = int(input_pos[-1]) + 1
+    logits = model(x, input_pos, _length)
     return sample(logits, **sampling_kwargs)[0]
 
-def decode_one_token(model: Transformer, x: torch.Tensor, input_pos: torch.Tensor, **sampling_kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
+def decode_one_token(model: Transformer, x: torch.Tensor, input_pos: torch.Tensor, _length, **sampling_kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
     # input_pos: [B, 1]
     assert input_pos.shape[-1] == 1
-    logits = model(x, input_pos)
+    logits = model(x, input_pos, _length)
     return sample(logits, **sampling_kwargs)
 
-def decode_n_tokens(model: Transformer, cur_token: torch.Tensor, input_pos: torch.Tensor, num_new_tokens: int, callback=lambda _: _, **sampling_kwargs):
+def decode_n_tokens(model: Transformer, cur_token: torch.Tensor, input_pos: torch.Tensor, num_new_tokens: int, callback=lambda _: _, dynamic_length_multiple=0, **sampling_kwargs):
     new_tokens, new_probs = [], []
+    _length = 0
     for i in range(num_new_tokens):
 
         # This is not working well dynamic length computation and make it much slower
         # with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_mem_efficient=False, enable_math=True): # Actually better for Inductor to codegen attention here
 
+        _length = int(input_pos) + 1
+        if dynamic_length_multiple > 0:
+            _length = (_length // dynamic_length_multiple + int(_length % dynamic_length_multiple)) * dynamic_length_multiple
+
         next_token, next_prob = decode_one_token(
-            model, cur_token, input_pos, **sampling_kwargs
+                model, cur_token, input_pos, _length, **sampling_kwargs
         )
         next_token = next_token.to(dtype=torch.int)
 
@@ -150,6 +160,7 @@ def generate(
     draft_model: Transformer,
     speculate_k: Optional[int] = 8,
     callback = lambda x: x,
+    dynamic_length_multiple: int = 0,
     **sampling_kwargs
 ) -> torch.Tensor:
     """
@@ -204,7 +215,7 @@ def generate(
             input_pos = input_pos + num_added
             next_token = next_tokens[-1]
     else:
-        generated_tokens, _ = decode_n_tokens(model, next_token.view(1, -1), input_pos, num_new_tokens - 1, callback=callback, **sampling_kwargs)
+        generated_tokens, _ = decode_n_tokens(model, next_token.view(1, -1), input_pos, num_new_tokens - 1, callback=callback, dynamic_length_multiple=dynamic_length_multiple, **sampling_kwargs)
         seq[T + 1:] = torch.cat(generated_tokens)
 
     generate_stats = {
@@ -279,6 +290,7 @@ def main(
     draft_checkpoint_path: Optional[Path] = None,
     speculate_k: int = 5,
     device=default_device,
+    dynamic_length_multiple: int = 0,
 ) -> None:
     """Generates text samples based on a pre-trained Transformer model and tokenizer.
     """
@@ -385,6 +397,7 @@ def main(
                 speculate_k=speculate_k,
                 interactive=interactive,
                 callback=callback,
+                dynamic_length_multiple=dynamic_length_multiple,
                 temperature=temperature,
                 top_k=top_k,
             )
@@ -441,10 +454,11 @@ if __name__ == '__main__':
     parser.add_argument('--speculate_k', type=int, default=5, help='Speculative execution depth.')
     parser.add_argument('--draft_checkpoint_path', type=Path, default=None, help='Draft checkpoint path.')
     parser.add_argument('--device', type=str, default=default_device, help='Device to use')
+    parser.add_argument('--dynamic_length_multiple', type=int, default=0, help='The multiple to be used for dynamic length multiplication.')
 
     args = parser.parse_args()
     main(
         args.prompt, args.interactive, args.num_samples, args.max_new_tokens, args.num_new_tokens, args.top_k,
         args.temperature, args.checkpoint_path, args.compile, args.compile_prefill, args.profile, args.draft_checkpoint_path,
-        args.speculate_k, args.device
+        args.speculate_k, args.device, args.dynamic_length_multiple,
     )
